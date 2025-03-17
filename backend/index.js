ed2d632de2eccfe6930120f0ad28610d20182131
tanman123kap <tanman123kap@gmail.com>
2025-03-17 11:32:38 +0530

    File change

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/products", productRoutes);
 
-if(process.env.NODE_ENV === "production ") {
+if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "..", "frontend", "dist", "index.html"));
